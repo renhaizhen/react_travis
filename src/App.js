e7d3451b71f5492b9652abb9e70837fe1a3cb50d
R_heson <heson@R-hesondeMBP.lan>
2021-04-13 15:03:55 +0800

    _j

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Travis react App
         </a>
       </header>
     </div>
